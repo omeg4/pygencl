10722a12b1190f5008e635575f313be32bf0f173
omeg4 <matthew.brunetti28@gmail.com>
2019-10-31 14:59:07 -0400

    minor formatting
    git-ignoring my templates and generated letters, just leaving the python code
    
    looks like everything is OK
    
    added README
    
    specified .md for README
    
    added a meta cover letter template

---
+++ README.md
@@ -0,0 +1,16 @@
+It is common practice amongst job-seekers to create one or more cover letter templates, changing only the company name and job title for each new application.
+
+But even this system requires several trivial-but-tedious steps to complete, which can disrupt the flow that one seeks to attain when applying to job after job for an extended period of time.
+
+So, here's a little Python script that prompts the user for the company name and job title, and also allows for choosing between multiple templates.
+After entering the relevant information at these prompts, the script generates a new .tex file in the subdirectory `autogen` and compiles that file with `pdflatex`.
+The newly generated PDF is then be copied elsewhere on the user's system - for example a job-application-specific folder that the user accesses frequently.
+
+For the most streamlined experience, create an alias to run the script from any directory:
+
+> `echo " alias pygencl='cd ~/RepoPath && python3 gencl.py'" >> ~/.zshrc`
+
+*Note*: It is important to execute `cd ~/RepoPath && python3 gencl.py`, since the script is written using relative directories, so that the `cwd` must be `~/RepoPath`.
+
+TODO:
+	* Replace my personal Cover Letters with dummy files for illustrative purposes.
--- gencl.py
+++ gencl.py
@@ -3,8 +3,10 @@ import subprocess as sp
 from datetime import datetime
 import string
 
+homedr = os.getenv('HOME')
 gendir = 'autogen/'
-cldict = { 'ds' : 'bash-datsci.tex', 'fin' : 'bash-finance.tex' }
+pdfdir = homedr + '/datadrop/Dropbox/jobapps/covletts' # Modify as necessary
+cldict = { 'ds' : 'bash-datsci.tex', 'fin' : 'bash-finance.tex', 'example' : 'template-example.tex' } # Modify as necessary
 
 def fnameformat(inarg):
     '''
@@ -13,15 +15,20 @@ def fnameformat(inarg):
     OUTPUT: CamelCaseArgum with punctuation removed and each word in the string shortened to 5 chars at most.
     '''
     fnamepart = []
+
     # for either a job title or company name, ensure Title Case and split each word
     for s in inarg.title().split():
+
         # Strip punctuation (if necessary)
         s = s.translate(str.maketrans("", "", string.punctuation))
+
         # Shorten each Word (if necessary)
         if len(s) > 5:
             s = s[:5]
+
         # Put each processed word into a list
         fnamepart.append(s)
+
     # After processing each word from the input argument, join() the elements in the list
     return ''.join(fnamepart)
 
@@ -41,6 +48,7 @@ def gencl(cltdkey, compname, jobtitle):
         True (if everything worked)
         [some error string] (if an exception was caught)
     '''
+
     # Lines of Latex code that define the company name and job title.
     # Easier to do it this way than to try to string.format() a tex file with a million curly braces
     cncommand = '\\newcommand{\\company}{%COMPNAME%\\ }'.replace('%COMPNAME%', compname)
@@ -48,27 +56,31 @@ def gencl(cltdkey, compname, jobtitle):
     with open(cldict[cltdkey], 'r') as fl:
         texfile = fl.read()
     texfile = texfile.replace('%COMPNAMECOMMAND%', cncommand).replace('%JOBTITLECOMMAND%', jtcommand)
-    # Generate a unique file name, something like {date}_{CompName}_{JobTitl}
+
+    # Generate a unique file name, something like {date}_{CompName}_{JobTitle}
     fname = datetime.now().strftime('%m-%d') + '_' + fnameformat(compname) + '_' + fnameformat(jobtitle)
+
     # Save the new tex file:
     # Also, we're gonna stuff all the .tex files and such in a subdir. the dir for generated files is `gendir = 'autogen/'`
     with open(gendir + fname + '.tex', 'w') as f:
         f.write(texfile)
+
     # call `pdflatex` on the newly saved file
     try:
         sp.run(['pdflatex', fname + '.tex'], cwd=gendir, check=True)
-    except CalledProcessError:
+    except:
         return 'something went wrong during compilation!'
 
     # copy the pdf to dropbox
     try:
-        sp.run(['cp', fname + '.pdf', '/home/matt/datadrop/Dropbox/jobapps/covletts/' + fname + '.pdf'], cwd=gendir, check=True)
-    except CalledProcessError:
-        return 'something went wrong when moving the PDF to dropbox!'
+        sp.run(['cp', fname + '.pdf', pdfdir + fname + '.pdf'], cwd=gendir, check=True)
+    except:
+        return 'something went wrong when moving the PDF!'
+
     return True
 
 if __name__ == '__main__':
     cname = input('Enter company name: ')
     title = input('Enter job title: ')
-    cldictkey = input('Cover letter dict key?\nds for data science template\nfin for finance template\n>> ')
+    cldictkey = input('Cover letter dict key? cldict = ' + str(cldict) + '\n>>')
     gencl(cldictkey, cname, title)
